d0efc01de218cbf76fecd5f11424daf000d82a5b
zuorn <zuorn@qq.com>
2019-04-23 00:34:06 +0800

    demo

--- Chapter/src/main/java/com/course/testng/BasicAnnotation.java
+++ Chapter/src/main/java/com/course/testng/BasicAnnotation.java
@@ -3,8 +3,10 @@ package com.course.testng;
 import org.testng.annotations.Test;
 
 public class BasicAnnotation {
-    @Test
+    //最基本的注解
+   @Test
     public void testCase1(){
+        System.out.println("这是测试用例1");
 
     }
 }
